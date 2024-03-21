2f1951477f54320845e986169e9aff75bb6bd51e
Diego Reis <diego.varella@globalfinanceiro.com.br>
2024-03-21 17:21:54 -0300

    adicionando infra do docker

--- src/main/java/com/example/shop/domains/User/UserRepository.java
+++ src/main/java/com/example/shop/domains/User/UserRepository.java
@@ -4,7 +4,9 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     UserDetails findByEmail(String email);
 }
